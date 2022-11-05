9b31e9a5be52451748f5a2eb3f2a88fbead49d36
dan <danstevethomas@gmail.com>
2022-11-04 23:50:27 -0300

    undo

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-var logger = require('./logger');
-
 
 module.exports = {
     data : new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Pong!'),
     async execute(interaction) {
-        logger.info('Ping command called by ${interaction.author.username}')
         interaction.reply({
             content: 'Pong!',
             ephemeral: false
