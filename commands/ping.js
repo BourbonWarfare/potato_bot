d6d2a66cab8fe28980b2ab24a82629d170a31a6d
dan <danstevethomas@gmail.com>
2022-11-05 00:10:21 -0300

    printf

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ module.exports = {
         .setName('ping')
         .setDescription('Pong!'),
     async execute(interaction) {
-        logger.info('[${interaction.option.name}] called by [${interaction.user.username}]')
+        logger.info('[{}] called by [{}]', interaction.option.name, interaction.user.username)
         interaction.reply({
             content: 'Pong!',
             ephemeral: false
