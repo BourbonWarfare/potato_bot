9c7e4c08ad1054ca41318535d4bef84e0cda1361
dan <danstevethomas@gmail.com>
2022-11-04 23:35:50 -0300

    test

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,7 @@ module.exports = {
         .setName('ping')
         .setDescription('Pong!'),
     async execute(interaction) {
+        logger.info('Ping command called by ${interaction.author.username}')
         interaction.reply({
             content: 'Pong!',
             ephemeral: false
