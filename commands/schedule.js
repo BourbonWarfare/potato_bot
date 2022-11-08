618bdc9a592264f22a1ffb3121694ce77e72251a
dan <danstevethomas@gmail.com>
2022-11-07 21:54:45 -0400

    onthaoe

---
+++ commands/schedule.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+var logger = require('../logger');
+
+module.exports = {
+    data : new SlashCommandBuilder()
+        .setName('schedule')
+        .setDescription('Pong!'),
+    async execute(interaction) {
+        logger.info('[',interaction.commandName,'] called by [', interaction.user.username, ']');
+        interaction.reply({
+            content: await tools.randomQuote,
+            ephemeral: false
+        });
+    }
+};
