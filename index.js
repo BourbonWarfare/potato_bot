336684930d20586b74106cfe75362cdab264f70e
dan <danstevethomas@gmail.com>
2022-11-07 22:52:23 -0400

    asntoeh

--- index.js
+++ index.js
@@ -20,12 +20,19 @@ const commands = [];
 client.commands = new Collection();
 client.tools = require('./tools/bad.js');
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-    client.commands.set(command.data.name, command);
-}
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		logger.error(`The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 client.once('ready', () => {
     logger.info("POTATO is Online");
