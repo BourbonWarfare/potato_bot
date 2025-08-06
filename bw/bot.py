1484b85080eb0777fe51d61531f93f2ac368c1a1
Bailey Danyluk <baileydanyluk@gmail.com>
2025-08-06 17:33:31 -0600

    add cogs to bot

--- bw/bot.py
+++ bw/bot.py
@@ -3,6 +3,8 @@ from typing import Self
 import logging
 import discord
 
+from bw.commands import community, helpers, mission_making, recruitment
+
 logger = logging.getLogger('bw.potbot')
 
 
@@ -16,7 +18,12 @@ class PotatoBot(discord.Client):
         )
 
     async def on_setup(self):
-        logger.info(f'Setup as {self.user}')
+        await self.add_cog(community.Community(self))
+        await self.add_cog(helpers.Helpers(self))
+        await self.add_cog(mission_making.MissionMaking(self))
+        await self.add_cog(recruitment.Recruitment(self))
+
+        logger.info(f'Setup as {self.user}. Ready to go! :3')
 
     async def on_ready(self):
         logger.info(f'Session ready for {self.user}')
