43c55ac336e1435a9b06a861a35966d2e0d7e19a
Bailey Danyluk <baileydanyluk@gmail.com>
2026-02-24 17:16:00 -0700

    dont dfefer twice

--- bw/commands/utils.py
+++ bw/commands/utils.py
@@ -46,5 +46,6 @@ async def get_session(interaction: discord.Interaction) -> tuple[BwSession, OAut
             SessionApi().revoke_user_session(State.state, interaction.user.id)
             bw_session, oauth_session = await show_login()
 
-    await interaction.response.defer()
+    if not interaction.is_done():
+        await interaction.response.defer()
     return bw_session, oauth_session
