15e4604554d2ab936e0eadce0292cf791cb409d4
Bailey Danyluk <baileydanyluk@gmail.com>
2026-02-24 20:00:32 -0700

    wrong iteration type

--- bw/commands/staff.py
+++ bw/commands/staff.py
@@ -230,7 +230,7 @@ class Staff(commands.Cog, name='Staff Commands'):
                         startup_status=server_status.get('startup_status', 'Unknown'),
                     ))
                 mod_update_log = []
-                for mod in mods.items():
+                for mod in mods:
                     mod_update_log.append(f'{mod.get('title', 'Unknown')}({mod.get('workshop_id', 'No Workshop ID')})')
                 await interaction.followup.send(f'Mods Updated:\n```{"\n".join(mod_update_log)}```')
             else:
