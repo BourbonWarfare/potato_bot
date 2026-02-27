14e8abb912b522eb7b266aff291b43d383087c7d
Bailey Danyluk <baileydanyluk@gmail.com>
2026-02-26 20:38:10 -0700

    only use lazy init

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import bw.log
 import logging.config
 from bw.environment import ENVIRONMENT
 from bw.bot import PotatoBot
-from bw.state import State
 
 
 def main():
     logging.config.dictConfig(bw.log.config())
-    _state = State()
     client = PotatoBot.setup()
     client.run(ENVIRONMENT.discord_token())
 
