f2637b353cb6349b4d5d755c5c230d1ae280be66
Bailey Danyluk <baileydanyluk@gmail.com>
2026-02-26 18:08:15 -0700

    linting fixes

--- bw/commands/utils.py
+++ bw/commands/utils.py
@@ -1,3 +1,5 @@
+from sqlalchemy.sql.functions import user
+from bw.session.types import DiscordSnowflake
 import discord
 import time
 import logging
@@ -13,18 +15,19 @@ from bw.utils import levenshtein_distance
 logger = logging.getLogger('bw.potbot.command')
 
 async def get_session(interaction: discord.Interaction) -> tuple[BwSession, OAuthSession]:
+    user_id = DiscordSnowflake(str(interaction.user.id))
     async def show_login() -> tuple[BwSession, OAuthSession]:
         oauth_session = await Authentication(interaction).internal_login_oauth(interaction)
-        bw_session = SessionApi().get_bw_session_from_discord_id(State.state, interaction.user.id)
+        bw_session = SessionApi().get_bw_session_from_discord_id(State.state, user_id)
         return bw_session, oauth_session
 
     try:
         logger.info(f'Loading session for {interaction.user}')
-        oauth_session = SessionApi().get_discord_session_from_discord_id(State.state, interaction.user.id)
+        oauth_session = SessionApi().get_discord_session_from_discord_id(State.state, user_id)
         if oauth_session.is_expired():
             raise DiscordSessionExpired()
 
-        bw_session = SessionApi().get_bw_session_from_discord_id(State.state, interaction.user.id)
+        bw_session = SessionApi().get_bw_session_from_discord_id(State.state, user_id)
         if bw_session.is_expired():
             raise BwSessionExpired()
     except NoSuchSession:
@@ -37,16 +40,16 @@ async def get_session(interaction: discord.Interaction) -> tuple[BwSession, OAut
             bw_session = await SessionApi().login_to_backend(State.state, oauth_session)
         except (RefreshFailed, CannotLogin) as e:
             logger.info(f'Could not refresh session: {e}. Re-logging in')
-            SessionApi().revoke_user_session(State.state, interaction.user.id)
+            SessionApi().revoke_user_session(State.state, user_id)
             bw_session, oauth_session = await show_login()
     except BwSessionExpired:
         logger.info(f'BW Session expired for {interaction.user}, attemptign to re-login')
-        oauth_session = SessionApi().get_discord_session_from_discord_id(State.state, interaction.user.id)
+        oauth_session = SessionApi().get_discord_session_from_discord_id(State.state, user_id)
         try:
             bw_session = await SessionApi().login_to_backend(State.state, oauth_session)
         except CannotLogin:
             logger.warning('Cannot login, retrying login')
-            SessionApi().revoke_user_session(State.state, interaction.user.id)
+            SessionApi().revoke_user_session(State.state, user_id)
             bw_session, oauth_session = await show_login()
 
     if not interaction.response.is_done():
