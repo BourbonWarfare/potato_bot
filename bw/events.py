736552c215c62f728e9424693f6d453022a0bf0e
Bailey Danyluk <baileydanyluk@gmail.com>
2026-04-08 08:32:18 -0600

    add print logging

--- bw/events.py
+++ bw/events.py
@@ -35,11 +35,14 @@ class Broker:
     async def backend_event_handler(self):
         timeout = aiohttp.ClientTimeout(total=None, sock_read=None)
         url = Interface().url(Root.get().api.v1.realtime.sse.resolve())
+        print('start', url, timeout)
         async with aiohttp.ClientSession(timeout=timeout) as session:
             async with session.get(url=url, headers={'Accept': 'text/event-stream'}) as response:
+                print('response')
                 response.raise_for_status()
                 async for line in response.content:
                     line = line.decode('utf-8').strip()
                     print(line)
+        print('end')
 
 global_event_broker = Broker()
