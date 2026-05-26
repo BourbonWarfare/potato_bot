7755a344511521da8b5a6ac0734814a695c90eaf
Bailey Danyluk <baileydanyluk@gmail.com>
2026-05-26 00:59:23 -0600

    fix coerce types method

--- bw/missions/response.py
+++ bw/missions/response.py
@@ -1,13 +1,35 @@
 import dataclasses
+import datetime
+import typing
 from bw.missions.types import MissionTypeTag, IterationUuid, UserUuid
 from dataclasses import dataclass
 from typing import Any
-from pydoc import locate
-import datetime
 
 
 @dataclass
-class MissionUploadResponse:
+class _CoerceFields:
+    def __post_init__(self):
+        hints = typing.get_type_hints(type(self))
+        for field in dataclasses.fields(self):
+            value = getattr(self, field.name)
+            field_type = hints[field.name]
+            check_type = typing.get_origin(field_type) or field_type
+
+            if isinstance(value, check_type):
+                continue
+
+            if check_type is datetime.datetime and isinstance(value, str):
+                value = datetime.datetime.fromisoformat(value)
+            elif dataclasses.is_dataclass(check_type) and isinstance(value, dict):
+                value = check_type(**value)
+            else:
+                value = check_type(value)
+
+            setattr(self, field.name, value)
+
+
+@dataclass
+class MissionUploadResponse(_CoerceFields):
     iteration_number: int
     min_players: int
     max_players: int
@@ -15,32 +37,16 @@ class MissionUploadResponse:
     safe_start_length: int
     mission_length: int
 
-    def __post_init__(self):
-        for field in dataclasses.fields(self):
-            value = getattr(self, field.name)
-            field_type = field.type if not isinstance(field.type, str) else locate(field.type)
-            assert isinstance(field_type, type)
-            if not isinstance(value, field_type):
-                setattr(self, field.name, field_type(value))
-
 
 @dataclass
-class MissionTypeResponse:
+class MissionTypeResponse(_CoerceFields):
     name: str
     signoffs_required: int
     tag: MissionTypeTag
 
-    def __post_init__(self):
-        for field in dataclasses.fields(self):
-            value = getattr(self, field.name)
-            field_type = field.type if not isinstance(field.type, str) else locate(field.type)
-            assert isinstance(field_type, type)
-            if not isinstance(value, field_type):
-                setattr(self, field.name, field_type(value))
-
 
 @dataclass
-class MissionInformationResponse:
+class MissionInformationResponse(_CoerceFields):
     uuid: IterationUuid
     server: str
     creation_date: datetime.datetime
@@ -50,17 +56,9 @@ class MissionInformationResponse:
     mission_type: MissionTypeResponse
     special_flags: dict[str, Any]
 
-    def __post_init__(self):
-        for field in dataclasses.fields(self):
-            value = getattr(self, field.name)
-            field_type = field.type if not isinstance(field.type, str) else locate(field.type)
-            assert isinstance(field_type, type)
-            if not isinstance(value, field_type):
-                setattr(self, field.name, field_type(value))
-
 
 @dataclass
-class IterationInformationResponse:
+class IterationInformationResponse(_CoerceFields):
     uuid: IterationUuid
     mission: MissionInformationResponse
     min_player_count: int
@@ -72,11 +70,3 @@ class IterationInformationResponse:
     bwmf_version: str
     iteration: int
     changelog: str
-
-    def __post_init__(self):
-        for field in dataclasses.fields(self):
-            value = getattr(self, field.name)
-            field_type = field.type if not isinstance(field.type, str) else locate(field.type)
-            assert isinstance(field_type, type)
-            if not isinstance(value, field_type):
-                setattr(self, field.name, field_type(value))
