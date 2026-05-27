b288efad4713d4145dfecea2a8a798d32c309d89
Bailey Danyluk <baileydanyluk@gmail.com>
2026-05-27 02:02:51 -0600

    Add logic for error on sse

--- bw/events/broker.py
+++ bw/events/broker.py
@@ -58,34 +58,46 @@ class Broker:
                 logger.error(f'Failed to run event handler: {str(e)}')
                 logger.debug(traceback.format_exc())
 
+    async def _get_sse(self, session: aiohttp.ClientSession, tasks: asyncio.TaskGroup):
+        timeout = aiohttp.ClientTimeout(total=None, sock_read=None)
+        url = server_url(Root.get().api.v1.realtime.sse.resolve())
+        headers = {hdrs.ACCEPT: 'text/event-stream', hdrs.CACHE_CONTROL: 'no-cache'}
+
+        async with session.get(url=url, timeout=timeout, headers=headers) as response:
+            try:
+                response.raise_for_status()
+            except aiohttp.ClientResponseError as err:
+                logging.warning(f'An error occured while retrieving event: {err}')
+                return
+
+            if response.status == 204:
+                logger.info('SSE stream has no content')
+                return
+
+            latest_event = ServerSentEventBuilder()
+            async for line_in_bytes in response.content:
+                line = line_in_bytes.decode('utf-8').strip('\n').strip('\r')
+                if line == '':
+                    tasks.create_task(self.publish(latest_event.finish()))
+                    latest_event = ServerSentEventBuilder()
+                    continue
+
+                prefix, following = line.split(':', 1)
+                if prefix == 'id':
+                    latest_event.with_id(following.strip())
+                elif prefix == 'event':
+                    latest_event.with_event(following.strip())
+                elif prefix == 'data':
+                    latest_event.with_data(json.loads(following.strip()))
+
     @tasks.loop()
     async def backend_event_handler(self):
         async with asyncio.TaskGroup() as tasks:
-            timeout = aiohttp.ClientTimeout(total=None, sock_read=None)
-            url = server_url(Root.get().api.v1.realtime.sse.resolve())
-            headers = {hdrs.ACCEPT: 'text/event-stream', hdrs.CACHE_CONTROL: 'no-cache'}
             async with aiohttp.ClientSession() as session:
-                async with session.get(url=url, timeout=timeout, headers=headers) as response:
-                    response.raise_for_status()
-                    if response.status == 204:
-                        logger.info('SSE stream has no content')
-                        return
-
-                    latest_event = ServerSentEventBuilder()
-                    async for line_in_bytes in response.content:
-                        line = line_in_bytes.decode('utf-8').strip('\n').strip('\r')
-                        if line == '':
-                            tasks.create_task(self.publish(latest_event.finish()))
-                            latest_event = ServerSentEventBuilder()
-                            continue
-
-                        prefix, following = line.split(':', 1)
-                        if prefix == 'id':
-                            latest_event.with_id(following.strip())
-                        elif prefix == 'event':
-                            latest_event.with_event(following.strip())
-                        elif prefix == 'data':
-                            latest_event.with_data(json.loads(following.strip()))
+                try:
+                    await self._get_sse(session, tasks)
+                except aiohttp.ClientConnectionError as err:
+                    logging.warning(f'Cannot connect to backend: {err}')
 
 
 global_event_broker = Broker()
