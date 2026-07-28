93ad6685beebc0d6522d98e3513f792922d19ec5
Bailey Danyluk <baileydanyluk@gmail.com>
2026-07-28 17:11:58 -0600

    change ordering

--- bw/commands/staff.py
+++ bw/commands/staff.py
@@ -40,10 +40,10 @@ class Staff(commands.Cog, name='Staff Commands'):
         name='arma',
         description='Manage an ARMA server.',
     )
+    @app_commands.autocomplete(server=arma_servers_autocomplete)
     @app_commands.choices(
         option=[app_commands.Choice(name=choice.value, value=choice.value) for choice in ArmaCommand],
     )
-    @app_commands.autocomplete(server=arma_servers_autocomplete)
     @app_commands.describe(
         server='The server which to perform the operation on.', option='The operation you wish to perform on the server.'
     )
